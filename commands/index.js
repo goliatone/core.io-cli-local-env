c70fd1e94ff97015896406681d09f8bc19f3f5a3
goliatone <burgosemi@gmail.com>
2017-10-23 22:52:18 -0400

    enable open share and list commands

--- commands/index.js
+++ commands/index.js
@@ -8,8 +8,11 @@ const Share = require('./share');
 const Start = require('./start');
 const Stop = require('./stop');
 const Uninstall = require('./uninstall');
+const Open = require('./open');
 
 module.exports.attach = function(prog) {
     Install.attach(prog);
     List.attach(prog);
+    Open.attach(prog);
+    Share.attach(prog);
 };
