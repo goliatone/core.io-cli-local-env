d4e4a3d1cb401f68b36de38fa267113df1c0317a
goliatone <burgosemi@gmail.com>
2017-10-23 23:06:21 -0400

    add stop command

---
+++ commands/stop.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const BaseCommand = require('./base');
+const CaddyInstaller = require('../lib/installers/caddy');
+const DnsmasqInstaller = require('../lib/installers/dnsmasq');
+
+class RestartCommand extends BaseCommand {
+
+    execute(args){
+        const logger = this.logger;
+        const caddy = new CaddyInstaller({logger});
+        const dnsmasq = new DnsmasqInstaller({logger});
+
+        return Promise.all([
+            caddy.stop(),
+            dnsmasq.stop()
+        ]);
+    }
+
+    static attach(prog){
+        const cmd = super.attach(prog);
+
+        cmd.action((args, options, logger)=>{
+            const command = new RestartCommand({
+                logger
+            });
+            args.options = options;
+            command.ready()
+                .execute(args)
+                .then((context)=>{
+                    process.exit(0);
+                });
+        });
+    }
+}
+
+RestartCommand.COMMAND_NAME = 'stop';
+RestartCommand.DESCRIPTION = 'Stop Caddy and Dnsmasq services';
+
+module.exports = RestartCommand;
