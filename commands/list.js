0bf2292bcba23c59e52d7b0072f7c8b025c0984c
goliatone <burgosemi@gmail.com>
2017-10-23 22:52:33 -0400

    implement command list

--- commands/list.js
+++ commands/list.js
@@ -1,9 +1,54 @@
 'use strict';
 
 const BaseCommand = require('./base');
+const fsu = require('../lib/fs-utils');
+const Table = require('cli-table-redemption');
 
 class ListCommand extends BaseCommand {
 
+    execute(args){
+        return new Promise((resolve)=>{
+            this.loadMetaFile().then((hosts)=>{
+                return this.listHosts(hosts);
+            }).then(resolve);
+        });
+    }
+
+    loadMetaFile(){
+        const metafile = this.paths.metafile;
+        return fsu.readFile(metafile, 'utf-8').then((content='')=>{
+            return JSON.parse(content);
+        });
+    }
+
+    listHosts(hosts=[]){
+        const table = new Table({
+            head: ['Name', 'Proxy', 'Domain'],
+            colWidths: [30, 60, 60]
+        });
+        return new Promise((resolve)=>{
+            hosts.map((host, i)=>{
+                table.push([host.name, host.proxy, host.domain]);
+            });
+            this.logger.info(table.toString());
+            resolve();
+        });
+    }
+
+    static attach(prog){
+        const cmd = super.attach(prog);
+        cmd.action((args, options, logger)=>{
+            const command = new ListCommand({
+                logger
+            });
+            args.options = options;
+            command.ready()
+                .execute(args)
+                .then((context)=>{
+                    process.exit(0);
+                });
+        });
+    }
 }
 
 ListCommand.COMMAND_NAME = 'list';
