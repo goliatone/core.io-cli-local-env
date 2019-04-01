ac019bcc4e5c0c77bf21484b8d7dac6351c39855
goliatone <burgosemi@gmail.com>
2019-03-31 20:31:46 -0700

    add mkdirp to host dir

--- commands/list.js
+++ commands/list.js
@@ -7,28 +7,37 @@ const Table = require('cli-table-redemption');
 
 class ListCommand extends BaseCommand {
 
-    execute(args){
-        return new Promise((resolve)=>{
-            this.loadMetaFile().then((hosts)=>{
+    execute(args) {
+        return new Promise(resolve => {
+            this.loadMetaFile().then(hosts => {
                 return this.listHosts(hosts);
             }).then(resolve);
         });
     }
 
-    loadMetaFile(){
+    loadMetaFile() {
         const metafile = this.paths.metafile;
-        return fsu.readFile(metafile, 'utf-8').then((content='')=>{
-            return JSON.parse(content);
+        const hosts = this.paths.hosts();
+        return fsu.mkdirp(hosts).then(_ => {
+            return fsu.readFile(metafile, 'utf-8').then((content = '') => {
+                return JSON.parse(content);
+            }).catch(err => {
+                if (err.code !== 'ENOENT') {
+                    return err;
+                }
+                this.logger.info('No metafile has been created...');
+            });
         });
     }
 
-    listHosts(hosts=[]){
+    listHosts(hosts = []) {
         const table = new Table({
             head: ['Name', 'Proxy', 'Domain'],
             colWidths: [30, 60, 60]
         });
-        return new Promise((resolve)=>{
-            hosts.map((host, i)=>{
+
+        return new Promise(resolve => {
+            hosts.map((host, i) => {
                 table.push([host.name, host.proxy, host.domain]);
             });
             this.logger.info(table.toString());
